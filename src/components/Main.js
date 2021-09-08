d5816b243b79e6d51351e10b1e71c3220aa70cca
rymbaniali <rym.ayasrah@hotmail.com>
2021-09-09 02:52:11 +0300

    add Form

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,36 +1,51 @@
 import React, { Component } from 'react';
 import Data from './data.json';
 import SelectedBeast from './SelectedBeast';
-
-
+import BsForm from './BsForm';
 
 
 class Main extends Component {
 
+    constructor(props){
+        super(props);
+        this.state={
+            speciality:''
+        }
+    }
 
+    handleSelect = (e) => {
+        let speciality = e.target.value;
+        this.setState({
+          speciality: speciality
+        })
+    }
     render() {
 
         return (
             <>
                 <h1>Horned Beasts </h1>
-                <p> displays images and information of horned animals.
+                <p> 
+                    displays images and information of horned animals.
                     This application will allow you to filter the images by
                     number of horns and chose your favorite image.</p>
 
+                    <BsForm
+                     handleSelect={this.handleSelect} />
+                    <br/>
+                <div className="row">
+                    {
 
-                <div className="row">{
                     Data.map(element => {
-                        return <SelectedBeast
+                       return  element.horns==this.state.speciality&&<SelectedBeast
                             horns={element.horns}
                             title={element.title}
                             descreption={element.description}
                             keyword={element.keyword}
                             img={element.image_url} />
-                    })
-
-                }
+                             }) 
+                    }
                 </div>
-
+                            
 
 
             </>
